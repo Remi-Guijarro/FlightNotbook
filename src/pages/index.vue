42cbff68c6bf1fa31a857e3c4239a0caa9f3c0cd
Remi-Guijarro <remi.guijarro@outlook.fr>
2021-05-20 23:16:43 +0200

    Initial commit

---
+++ src/pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="text-red-500">
+        seotnsohe
+    </div>
+</template>
+
+<script setup>
+    import { reactive } from 'vue'
+    import { useHead } from '@vueuse/head'
+
+    import packageJSON from '/src/../package.json'
+
+    useHead({
+        title: packageJSON.name,
+        meta: [
+            {
+                name: 'description',
+                content: packageJSON.description,
+            },
+        ],
+    })
+
+    const state = reactive({
+    })
+</script>
